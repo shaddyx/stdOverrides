17417d025af1302686ca2ce6ea301e58abd258c5
Anatolii-Yakushko-NOWP <Anatolii.Yakushko@nowpensions.com>
2021-12-04 13:35:01 +0200

    [+] added smart pointers

---
+++ src/pointers/RC.h
@@ -0,0 +1,19 @@
+class RC
+{
+    private:
+    int count; // Reference count
+
+    public:
+    void AddRef()
+    {
+        // Increment the reference count
+        count++;
+    }
+
+    int Release()
+    {
+        // Decrement the reference count and
+        // return the reference count.
+        return --count;
+    }
+};
