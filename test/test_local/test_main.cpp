3ad2584761e7350b57413f7ed96bf47cb74ac5a0
Anatolii-Yakushko-NOWP <Anatolii.Yakushko@nowpensions.com>
2021-12-25 18:52:40 +0200

    [-] removed str_util

--- test/test_local/test_main.cpp
+++ test/test_local/test_main.cpp
@@ -2,12 +2,10 @@
 #include "test_list.h"
 #include "test_promise.h"
 #include "test_pointers.h"
-#include "test_str.h"
 #include "test_list_destructors.h"
 int main(){
     test_list();
     test_promise();
     test_pointers();
-    test_str_main();
     test_list_destructors();
 }
